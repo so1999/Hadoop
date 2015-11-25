c7f7ef8be9c2f342f80706d1fa1f6b20171238e0
so1999 <s-c@outlook.com>
2015-11-25 17:13:10 +0800

    Reduce

--- wordcount/src/main/java/WordCount.java
+++ wordcount/src/main/java/WordCount.java
@@ -1,24 +1,15 @@
-import org.apache.hadoop.conf.Configuration;
+import java.io.IOException;
+import java.util.*;
+
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Iterator;
-import java.util.StringTokenizer;
-
-/**
- * Created by Sun on 2015/11/25.
- */
 public class WordCount {
     public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
         private final static IntWritable one = new IntWritable(1);
@@ -35,7 +26,7 @@ public class WordCount {
     }
 
     public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-        public void reduce(Text key, Iterable<IntWritable> values, Mapper.Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             int sum = 0;
             for (IntWritable val : values) {
                 sum += val.get();
@@ -44,9 +35,9 @@ public class WordCount {
         }
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = new Job(conf,"wordcount");
+        Job job = new Job(conf, "wordcount");
         job.setJarByClass(WordCount.class);
 
         job.setOutputKeyClass(Text.class);
@@ -58,8 +49,8 @@ public class WordCount {
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(TextOutputFormat.class);
 
-        FileInputFormat.addInputPath(job,new Path(args[0]));
-        FileOutputFormat.setOutputPath(job,new Path(args[1]));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.waitForCompletion(true);
     }
